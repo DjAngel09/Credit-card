0033f8397d9dc7615982a329e4f0284ac7d5187a
GFSSCORP\Z053023 <jhernandez@palo-it.com>
2021-03-17 12:02:51 -0600

    se termina los estilos para la tarjeta

--- src/App.js
+++ src/App.js
@@ -1,17 +1,32 @@
 import logo from './logo.svg';
 import './App.scss';
+import { useState } from 'react';
 
 function App() {
+
+ const [cvv, setCvv] = useState({showCvv:false});
+
+  const onFocus = () => {
+    setCvv({showCvv: true});
+    console.log(cvv);
+  }
+
+  const onFocusOut = () => {
+    setCvv({showCvv: false});
+  }
+
   return (
     <div className="App">
       <div className="card">
-        <div className="credit-card">
+        <div className={`credit-card ${cvv.showCvv ? 'active' : ''}`}>
           <div className="card-1">
             <p className="num-card">4152 3133 6182 75689</p>
             <p className="expired">23/21</p>
             <p className="name">Angel Hernandez Martinez</p>
           </div>
-          <div className="card-2"></div>
+          <div className="card-2">
+            <p className="cvv">***</p>
+          </div>
         </div>
         <form>
           <div className="cont-input">
@@ -47,7 +62,11 @@ function App() {
               <label htmlFor="">
                 Cvv
               </label>
-              <input type="text"/>
+              <input 
+                type="text" 
+                onFocus={onFocus}
+                onBlur={onFocusOut}
+              />
             </div>
           </div>
 
